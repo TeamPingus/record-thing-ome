d0ff74e43b1275e1f605d8cf66cff3ab01cccf2c
PassiHD <passihd@phoenixts.eu>
2025-02-27 20:09:25 +0100

    make code more betterer
    Signed-off-by: PassiHD <passihd@phoenixts.eu>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-extern crate getopts;
-
-use getopts::{HasArg, Occur, Options};
+use clap::Parser;
 use reqwest::Client;
 use serde::Serialize;
+use serde_json::Value;
+
+// arg struct
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long, help = "Start Recording")]
+    start_record: bool,
+    #[arg(short, long, help = "Stop Recording")]
+    end_record: bool,
+    #[arg(short, long, help = "Check Recording")]
+    check_record: bool,
+}
 
 // api stuff (send help btw)
 
@@ -13,66 +24,65 @@ struct Stream {
 }
 
 #[derive(Serialize, Debug)]
-struct RecordStopCheck {
-    id: String,
-}
-
-#[derive(Serialize, Debug)]
 struct RecordThingi {
     id: String,
     stream: Option<Stream>,
 }
 
 impl RecordThingi {
-    async fn start(url: String, token: String, id: String, name: String) -> Result<(), reqwest::Error> {
-        let client = Client::new();
+    async fn start(
+        url: String,
+        token: String,
+        id: String,
+        name: String,
+    ) -> Result<(), reqwest::Error> {
         let url = format!("{}/v1/vhosts/default/apps/app:startRecord", url);
         let stream = Stream { name };
-        let body = RecordThingi { id, stream: Some(stream) };
-        
-        let res = client
-            .post(url)
-            .header("Authorization", token)
-            .json(&body)
-            .send()
-            .await?;
+        let body = RecordThingi {
+            id,
+            stream: Some(stream),
+        };
 
-        println!("{}", res.text().await?);
+        Self::request(url, token, body).await?;
 
         Ok(())
-
     }
     async fn stop(url: String, token: String, id: String) -> Result<(), reqwest::Error> {
-        let client = Client::new();
         let url = format!("{}/v1/vhosts/default/apps/app:stopRecord", url);
-        let body = RecordStopCheck { id };
+        let body = RecordThingi { id, stream: None };
 
-        let res = client
-            .post(url)
-            .header("Authorization", token)
-            .json(&body)
-            .send()
-            .await?;
-
-        println!("{}", res.text().await?);
+        Self::request(url, token, body).await?;
 
         Ok(())
     }
     async fn check(url: String, token: String, id: String) -> Result<(), reqwest::Error> {
-        let client = Client::new();
         let url = format!("{}/v1/vhosts/default/apps/app:records", url);
-        let body = RecordStopCheck { id };
-        
+        let body = RecordThingi { id, stream: None };
+
+        Self::request(url, token, body).await?;
+
+        Ok(())
+    }
+
+    async fn request<T: Serialize>(
+        url: String,
+        token: String,
+        body: T,
+    ) -> Result<String, reqwest::Error> {
+        let client = Client::new();
         let res = client
             .post(url)
             .header("Authorization", token)
             .json(&body)
             .send()
+            .await?
+            .json::<Value>()
             .await?;
 
-        println!("{}", res.text().await?);
+        let res = serde_json::to_string_pretty(&res).unwrap();
+        println!("{}", res);
 
-        Ok(())
+        Ok(res)
     }
 }
 
@@ -82,82 +92,23 @@ async fn main() -> Result<(), reqwest::Error> {
     let token = format!("Basic {}", dotenvy::var("TOKEN").unwrap());
     let id = dotenvy::var("RECORD_ID").unwrap().to_string();
     let name = dotenvy::var("RECORD_NAME").unwrap().to_string();
-    // cli stuff
-    fn print_usage(program: &str, opts: Options) {
-        let brief = format!("Usage: {} [options]", program);
-        print!("{}", opts.usage(&brief));
+
+    let args = Cli::parse();
+
+    if args.start_record && args.end_record {
+        println!("Error: Cannot start and stop recording simultaneously.");
+        return Ok(());
     }
 
-    let args: Vec<_> = std::env::args().collect();
-    let program = args[0].clone();
-
-    let mut opts = Options::new();
-    opts.opt(
-        "1",
-        "start",
-        "start Recording",
-        "",
-        HasArg::No,
-        Occur::Optional,
-    );
-    opts.opt(
-        "2",
-        "stop",
-        "stop Recording",
-        "",
-        HasArg::No,
-        Occur::Optional,
-    );
-    opts.opt(
-        "3",
-        "status",
-        "see status of recording",
-        "",
-        HasArg::No,
-        Occur::Optional,
-    );
-    opts.opt(
-        "h",
-        "help",
-        "print this help",
-        "",
-        HasArg::No,
-        Occur::Optional,
-    );
-    opts.opt(
-        "c",
-        "config",
-        "config location",
-        "",
-        HasArg::No,
-        Occur::Optional,
-    );
-
-    let matches = match opts.parse(&args[..]) {
-        Ok(m) => m,
-        Err(f) => {
-            print!("hello");
-            panic!("{}", f.to_string())
-        }
-    };
-
-    if matches.opt_present("h") {
-        print_usage(&program, opts);
-    }
-
-    if matches.opt_present("1") {
+    if args.start_record {
         RecordThingi::start(url.clone(), token.clone(), id.clone(), name).await?;
     }
-    if matches.opt_present("2") {
+    if args.end_record {
         RecordThingi::stop(url.clone(), token.clone(), id.clone()).await?;
     }
-    if matches.opt_present("3") {
+    if args.check_record {
         RecordThingi::check(url, token, id).await?;
     }
-    if matches.opt_present("c") {
-        println!("Not implemented yet, deal with it");
-    }
+
     Ok(())
 }
-
-//
